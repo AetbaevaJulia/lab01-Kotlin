4c87c25dec86b1de8bfd7618619e9e17d92ad593
AetbaevaJulia <aetbaevajulia05@gmail.com>
2025-10-17 23:44:38 +0500

    add main fragment (interface)

---
+++ app/src/main/java/com/example/calculator/MainFragment.kt
@@ -0,0 +1,34 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.calculator.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+    private var binding: MainFragmentBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            MainFragment().apply {
+
+            }
+    }
+
+}
